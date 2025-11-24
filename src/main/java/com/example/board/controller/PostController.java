e591f01f12710c5ddc99e8a7f0da940bd48f72cc
stnguswnd <stnguswnd@gmail.com>
2025-11-24 12:43:11 +0900

    JPA 설정

---
+++ src/main/java/com/example/board/controller/PostController.java
@@ -0,0 +1,69 @@
+package com.example.board.controller;
+
+import com.example.board.dto.PostDto;
+import com.example.board.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostRepository postRepository;
+
+//    public PostController(PostRepository postRepository) {
+//        this.postRepository = postRepository;
+//    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("posts", postRepository.findAll());
+        return "posts/list";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        PostDto post = postRepository.findById(id);
+        model.addAttribute("post", post);
+        return "posts/detail";
+    }
+
+    @GetMapping("/new")
+    public String newPost(Model model) {
+        model.addAttribute("post", new PostDto());
+        return "posts/form";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute PostDto postDto) {
+        postRepository.save(postDto);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        PostDto post = postRepository.findById(id);
+        model.addAttribute("post", post);
+        return "posts/form";
+    }
+
+    @PostMapping("/{id}")
+    public String update(
+            @PathVariable Long id,
+            @ModelAttribute PostDto postDto) {
+        postRepository.update(id, postDto);
+        return "redirect:/posts/" + id;
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable Long id) {
+        postRepository.delete(id);
+        return "redirect:/posts";
+    }
+
+
+
+
+}
---
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -0,0 +1,64 @@
+package com.example.board.repository;
+
+import com.example.board.dto.PostDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    // @RequiredArgsConstructor를 통해 자동생성
+//    public PostRepository(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+
+    private final RowMapper<PostDto> rowMapper = (rs, rowNum) -> {
+        return new PostDto(
+                rs.getLong("id"),
+                rs.getString("title"),
+                rs.getString("content"),
+                rs.getTimestamp("created_at").toLocalDateTime()
+        );
+    };
+
+    // 전체 조회
+    public List<PostDto> findAll() {
+        String sql = "SELECT * FROM post";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    // 상세조회
+    public PostDto findById(Long id) {
+        String sql = "SELECT * FROM post WHERE id = ?";
+
+        // queryForObject => 단일 행 조회
+        PostDto post = jdbcTemplate.queryForObject(sql, rowMapper, id);
+
+        return post;
+    }
+
+    public void save(PostDto postDto) {
+        String sql = "INSERT INTO post (title, content) VALUES (?, ?)";
+        jdbcTemplate.update(sql, postDto.getTitle(), postDto.getContent());
+    }
+
+    public void update(Long id, PostDto postDto) {
+        String sql = "UPDATE post SET title = ?, content = ? WHERE id = ?";
+        jdbcTemplate.update(sql, postDto.getTitle(), postDto.getContent(), id);
+    }
+
+    public void delete(Long id) {
+        String sql = "DELETE FROM post WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+
+
+}
